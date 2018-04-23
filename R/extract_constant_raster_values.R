8b6c54dd3d8c4ab0a36078370b60c581eb579b41
Melamed <W0301302@campus.nscc.ca>
2018-04-23 10:20:07 -0300

    working away...

--- R/extract_constant_raster_values.R
+++ R/extract_constant_raster_values.R
@@ -3,26 +3,30 @@
 
 #' @param temperatures_df a dataframe of temperature data. Must contain 'stationid' field
 #' @param rasters_list a list of constant rasters used as input for modelling
+#' @param stations_sp a SpatialPointsDataFrame object of weather stations. names() must be "stationid"
+#' with AN1, AN2, AN3 ... etc.
 #' @return input temperatures_df with added columns, one for each raster in rasters_list
 #' @export
-extract_constant_raster_values <- function(temperatures_df, rasters_list){
+extract_constant_raster_values <- function(temperatures_df, rasters_list, stations = stations_sp){
   
   # Loop through each raster in list
   for(i in seq_along(rasters_list)){
+    
+    raster <- raster::projectRaster(from = rasters_list[[i]], crs = stations@proj4string)
     # Extract raster values to spatial points dataframe of SWNS stations
-    stations_sp$extract <- raster::extract(x = rasters_list[[i]],
+    stations$extract <- raster::extract(x = raster,
                                            y = stations_sp)
     # Rename extracted values column to the raster name
-    names(stations_sp)[names(stations_sp) == "extract"] <- names(rasters_list[[i]])
+    names(stations)[names(stations) == "extract"] <- names(raster)
   }
   # Coerce spatial points dataframe to dataframe, for joining
-  stations_sp_df <- as.data.frame(stations_sp) %>%
+  stations_df <- as.data.frame(stations) %>%
     dplyr::select(-EASTING, -NORTHING)  # remove unnecessary rows
-  stations_sp_df$stationid <- as.character(stations_sp_df$stationid) # coerce to char from factor
+  stations_df$stationid <- as.character(stations_df$stationid) # coerce to char from factor
   
   # Join extracted raster values at each station back to input temperatures table
   out_df <- dplyr::left_join(x = temperatures_df,
-                             y = stations_sp_df,
+                             y = stations_df,
                              by = "stationid")
   return(out_df)
 }
---
+++ R/extract_temporal_raster_values.R
@@ -0,0 +1,3 @@
+#' Extracts raster values from a set of rasters in the temporal_raster_df
+#' 
+#' 
