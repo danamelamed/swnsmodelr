092d1b69506f4a68bb32e000cc82463e1afc6469
Melamed <W0301302@campus.nscc.ca>
2018-06-07 15:42:55 -0300

    commit

--- R/extract_daily_spvs.R
+++ R/extract_daily_spvs.R
@@ -3,15 +3,14 @@
 extract_daily_spvs  <- function(df,
                              dep_vars,
                              ind_vars,
-                             years,
+                             year,
                              days){
   # get only specified year of data
   df <- dplyr::filter(df, year == 2012)
   
   df_yday_bind <- list()
   for(i in seq_along(days)){
-    
-    print(days[[i]])
+ 
     df_yday <- dplyr::filter(df, yday == days[[i]])
     
     dep_var_df <- list()
@@ -19,7 +18,10 @@ extract_daily_spvs  <- function(df,
       
       ind_var_df <- list()
       for(k in seq_along(ind_vars)){
+        
         ind_var_df[[k]] <- df_yday
+        
+        ind_var_df[[k]]$dep_var <- dep_vars[[j]]
         if(ind_vars[[k]] == "east,north"){
           formula = paste0(dep_vars[[j]],"~ s(east,north)")
           ind_var_df[[k]]$ind_var <- "(E,N)"
@@ -29,7 +31,7 @@ extract_daily_spvs  <- function(df,
           ind_var_df[[k]]$ind_var <- ind_vars[[k]]
         }
       
-        print(formula)
+       
         f <- as.formula(formula)
         
         m <- gam(f, data = df_yday)
@@ -37,7 +39,7 @@ extract_daily_spvs  <- function(df,
         
         
         ind_var_df[[k]]$pval <- summary.gam(m)$s.pv
-        ind_var_df[[k]]$dep_var <- dep_vars[[j]]
+        
       }
       dep_var_df[[j]] <- dplyr::bind_rows(ind_var_df)
     }
