b30be678c30b2322ab1d6326cb4b4336649726e5
dana9228 <DMelamed@esri.com>
2020-06-27 15:23:27 -0700

    update

--- R/predict_rasters.R
+++ R/predict_rasters.R
@@ -1,140 +1,127 @@
 #' Generate daily mean temperature raster with GAM
-predict_mean_daily_rasters <- function(start_date,
+predict_rasters <- function(start_date,
                                           end_date,
-                                          formula,
-                                          alt_formula,
+                                          #formula,
+                                          #alt_formula,
                                           temperatures_df,
-                                          raster_brick,
+                                          validation_df,
+                                          brick,
                                           temporal_rasters_df = 1,
                                           output_folder,
-                                          output_ext,
+                                          output_ext = ".tif",
                                           verbose = TRUE
                                           ){
 
-        
-        start_date = ymd('2017-01-26')
-        end_date   = ymd('2017-12-31')
-        
-        
-        
-        # Sample of using package
-        
-        
-        # filter model table
-        df <- filter(model_stations_df,
-                     date_time >= start_date &
-                       date_time <= end_date)
+        par(mfrow = c(2,2))
+        for (i in 1:(start_date-end_date)[[1]]){
+                date_now <- start_date  - 1 + i
+                
+                # subset stations by date
+                daily_df = temperatures_df %>% filter(date_time == date_now)
+                daily_val_df = validation_df %>% filter(date_time == date_now)
+                
+                
         
         
-        
-        
-        years <- 2017
-        n_years <- years %>% length()
-        
-        no_sol_model <- list()
-        
-        for(j in 1:n_years){
-          
-          df_year <- filter(df,
-                            year == years[[j]])
-          
-          days <- df_year$yday %>% unique() 
-          n_days <- length(days)
-          # loop over weeks
-          for(i in 1:n_days){
-            
-            ### GENERATE WEEKLY MODELS
-            
-            # Get weekly df
-            daily_df <- df_year %>% 
-              filter(yday == days[[i]])
-            print(i)
-            ### THE MODELS
-            # There were erroneous zeroes in sum_irradiance which were converted to NA.
-            # Because of that process, an error may error in generating the model.
-            # If it does, a back-up model without sum_irradiance is run
-            m <- try(gam(temp_mean ~
+                ### THE MODELS
+                # There were erroneous zeroes in sum_irradiance which were converted to NA.
+                # Because of that process, an error may error in generating the model.
+                # If it does, a back-up model without sum_irradiance is run
+                m <- try(gam(temp_mean ~
                            s(east,north) +
-                           s(dem) +
+                           s(east, k =3) +
+                           s(north, k= 3) +
+                           s(dem, k =3) +
                            s(sum_irradiance) +
-                          # s(tpi, k=9) +
-                           s(ptoc, k= 3) ,
-                          # s(asp_c, k= 9),
+                           s(tpi) +
+                           s(ptoc, k= 3) +
+                           s(asp, k= 3),
                          data = daily_df))
-            # Back-up model
-            if("try-error" %in% class(m)){
-              m <- gam(temp_mean ~
-                         s(east,north) +
-                         s(dem, k=9) +
-                         # s(sum_irradiance) +
-                        # s(tpi, k=9) +
-                         s(ptoc) ,
-                       #  s(asp_c, k= 9),
-                       data = daily_df)}
-            # No temporal raster available
-            m2 <- gam(temp_mean ~
-                        s(east,north) +
-                        s(dem) +
-                        #  s(sum_irradiance) +
-                      #  s(tpi, k=9) +
-                        s(ptoc, k=3) ,
-                     #   s(asp_c, k= 9),
-                      data = daily_df)
-            
-            if(verbose == TRUE)
-            print(summary(m))
-            print(summary(m2))
-            
-            
-            
-              
-              # The date of the current raster
-              date_now <- daily_df$date_time %>% unique()
-              print(date_now)
-              
-              # Make a raster brick for the prediction
-              brick <- brick(rasters_list)
-              brick <- brick_datetime_rasters(brick, rasters_list[[1]], date_now)
-              
-              
-              
-              # check for solar raster
-              solar_irradiance_now <- solar_irradiance_rasters_df %>%
-                filter(date_time == date_now)
-              
-              # Solar raster is available...
-              if(length(solar_irradiance_now[[1]] != 0)){
+                
+                
+                
+                # Back-up model
+                if("try-error" %in% class(m)){
+                        m <- gam(temp_mean ~
+                                       s(east,north) +
+                                       s(east, k =3) +
+                                       s(north, k= 3)+
+                                       s(dem, k =3) +
+                                       s(tpi) +
+                                       s(ptoc, k= 3) +
+                                       s(asp, k= 3),
+                               data = daily_df)}
+        
+                    
+                 if(verbose == TRUE){
+                   print(summary(m))}
+                   
+                    
+                    
+                    
+                
+                # check for solar raster
+                solar_irradiance_now <- solar_irradiance_rasters_df %>%
+                  filter(date_time == date_now)
+                
+                # Solar raster is available...
+                if(length(solar_irradiance_now[[1]] != 0)){
                 # add solar raster to brick
-                solar_raster <- raster(solar_irradiance_now[[1]][[1]])
-                names(solar_raster) <- "sum_irradiance"
-                brick <- raster::addLayer(brick, solar_raster)
+                  solar_raster <- raster(solar_irradiance_now[[1]][[1]])
+                  names(solar_raster) <- "sum_irradiance"
+                  brick_temp <- raster::addLayer(brick, solar_raster)
+                        
+                  # model over day
+                  prediction_raster <- predict(brick_temp, m)
+                  
+                  
+                  
+                        
+                  # output prediction temp mean raster
+                  writeRaster(prediction_raster, file.path(output_folder,
+                                                            paste0(date_now, output_ext)),
+                                  overwrite = TRUE)
+                  }else{
+                        
+                     # Solar raster available, run m as model
+                     prediction_raster <- predict(brick, m)
+                     
+                        
+                      # output prediction temp mean raster
+                      writeRaster(prediction_raster, file.path(output_folder,
+                                                               paste0(date_now, output_ext)),
+                                  overwrite = TRUE)
+                      
+                  }
                 
-                # model over day
-                prediction_raster <- predict(brick, m)
+                
+                daily_val_df <- add_predictions(daily_val_df, m)
+                daily_val_df <- add_residuals(daily_val_df, m)
+                
+                # reorder validation stations and give new ID
+                daily_val_df <- daily_val_df[order(daily_val_df$temp_mean),]
+                daily_val_df <- daily_val_df %>% mutate(ID = 1:n())
+                      
+                # plot predicted raster 
                 plot(prediction_raster)
+                # add validation stations
+                points(daily_val_df$east, daily_val_df$north, size = daily_val_df$resid)
+                text(daily_val_df$east, daily_val_df$north,labels= daily_val_df$ID)
                 
-                # output prediction temp mean raster
-                writeRaster(prediction_raster, paste0("Z:\\Dana\\Daily\\Daily_Temp_Mean_200_2\\temp_mean",
-                                                      date_now,".tif"),
-                            overwrite = TRUE)
-              }else{
                 
-                # Solar raster available, run m as model
-                prediction_raster <- predict(brick, m2)
-                plot(prediction_raster)
+                # plot actual and predicted validation values
                 
-                # output prediction temp mean raster
-                writeRaster(prediction_raster, paste0("Z:\\Dana\\Daily\\Daily_Temp_Mean_200_2\\temp_mean",
-                                                      date_now,".tif"),
-                            overwrite = TRUE)
-                no_sol_model[[length(no_sol_model) + 1]] <- date_now
-              }
+                
+                plot(daily_val_df$ID,daily_val_df$temp_mean)
+                points(daily_val_df$ID, daily_val_df$pred, pch=20 , col="red")
+                
+                
+                
               
-              
-          }
+}
           
           
-        }
+   
         
         
         
