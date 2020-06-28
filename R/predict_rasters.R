2102ee369ec817477e26c5a236ed550039ab6f6c
dana9228 <DMelamed@esri.com>
2020-06-27 19:37:18 -0700

    make fixes

--- R/predict_rasters.R
+++ R/predict_rasters.R
@@ -9,17 +9,26 @@ predict_rasters <- function(start_date,
                                           temporal_rasters_df = 1,
                                           output_folder,
                                           output_ext = ".tif",
-                                          verbose = TRUE
+                                          verbose = TRUE,
+                                          resid_plot = FALSE
                                           ){
 
-        par(mfrow = c(1,2))
-        for (i in 1:(start_date-end_date)[[1]]){
-                date_now <- start_date  - 1 + i
+        
+        
+        daily_val_df_out = validation_df %>% 
+                filter(as.character(date_time) == "impossible")
+        daily_val_df_month = validation_df %>% 
+                filter(as.character(date_time) == "impossible")
+        empty_df = validation_df %>% 
+                filter(as.character(date_time) == "impossible")
+        current_month = month(start_date)
+        for (i in 1:(end_date-start_date)[[1]]){
+                date_now <- (start_date + i) - 1 
                 
                 # subset stations by date
                 daily_df = temperatures_df %>% filter(date_time == date_now)
                 daily_val_df = validation_df %>% filter(date_time == date_now)
-                print(length(daily_df))
+                
                 
         
         
@@ -53,7 +62,7 @@ predict_rasters <- function(start_date,
                                data = daily_df)}
         
                     
-                 if(verbose == TRUE){
+                 if(verbose){
                    print(summary(m))}
                    
                     
@@ -100,28 +109,50 @@ predict_rasters <- function(start_date,
                 
                 # reorder validation stations and give new ID
                 daily_val_df <- daily_val_df[order(daily_val_df$temp_mean),]
-                daily_val_df <- daily_val_df %>% mutate(ID = 1:n())
-                      
+                daily_val_df <- daily_val_df %>% mutate(ID = 1:n()) %>%
+                        filter(!is.na(resid))
+                
+                
+                daily_val_df_out <- bind_rows(daily_val_df_out, daily_val_df)
+               
+               
+                
+
+                # plot rasters and residuals for each day
+                if(verbose){
                 # plot predicted raster 
-                plot(prediction_raster)
-                # add validation stations
-                points(daily_val_df$east, daily_val_df$north, size = daily_val_df$resid)
-                text(daily_val_df$east, daily_val_df$north,labels= daily_val_df$ID)
+                        par(mfrow = c(1,2))
+                        plot(prediction_raster)
+                        # add validation stations
+                        points(daily_val_df$east, daily_val_df$north, size = daily_val_df$resid)
+                        text(daily_val_df$east, daily_val_df$north,labels= daily_val_df$ID)
+                        
+                        
+                        # plot actual and predicted validation values
+                        plot(daily_val_df$ID,daily_val_df$temp_mean)
+                        points(daily_val_df$ID, daily_val_df$pred, pch=20 , col="red")
+                }
                 
-                
-                # plot actual and predicted validation values
-                
-                
-                plot(daily_val_df$ID,daily_val_df$temp_mean)
-                points(daily_val_df$ID, daily_val_df$pred, pch=20 , col="red")
-                
-                
-                
+                #plot residual box plots for each month
+                if(resid_plot){
+                   print(date_now)
+                   month_now <- month(date_now)
+                   daily_val_df_month <- bind_rows(daily_val_df_month, daily_val_df)
+                   if(month_now != current_month | date_now == (end_date-1)){
+                           boxplot(resid ~ date_time, daily_val_df_month, ylim = c(-5,5))
+                           current_month <- month_now
+                           daily_val_df_month <- empty_df
+                           
+                   }
+                        
+                        
+                        
+                }
               
 }
           
           
-   
+   return(daily_val_df_out)
         
         
         
