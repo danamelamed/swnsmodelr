1104bf86cab8949c894ef25ece3f716210357f2f
Dana Melamed <danamelamed@hotmail.com>
2022-06-20 20:39:35 -0400

    resample all GOES rasters

---
+++ R/Scripts/resample_GOES.R
@@ -0,0 +1,30 @@
+# resample solar irradiance rasters
+library(raster)
+# to 20m...
+output_folder <- 'D:\\GOES\\1000m'
+input_folder <- 'D:\\GOES\\700m'
+
+
+raster100m <- raster('D:\\data\\rasters\\input_rasters\\dem1000m.tif')
+
+
+
+input_files <- list.files(input_folder)
+
+
+
+for(tif in input_files){
+  if(grepl('.tif',tif)){
+    print(tif)
+    if(tif %in% list.files(output_folder)){
+      print('exists')
+    }else{
+     try(new_raster <- projectRaster(from = raster(file.path(input_folder,tif)),
+                                 to   = raster1000m,
+                                 method = 'bilinear',
+                                 filename = file.path(output_folder,tif),
+                                 overwrite = FALSE))}
+  }
+}
+
+
