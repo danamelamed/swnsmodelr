4e591c2640bfa1450fb9542c76e4e03167d2b98c
Dana Melamed <dana.melamed@kongsberggeospatial.com>
2023-05-07 14:59:06 -0400

    remove stuff im not using anymore

--- R/data.R
+++ R/data.R
@@ -1,21 +1,2 @@
-#' Weather station data in Southwest Nova Scotia
-#' 
-#' A dataset containing the weather station temperature recordings and coordinates
-#' 
-"swns_stations_df"
-
-
-#' Weather station data in Southwest Nova Scotia from 200m resolution
-#' 
-#' A dataset containing the weather station temperature recordings, coordinates, and 
-#' values extracted from environmental rasters at 200m resolution.
-#' 
-"swns_stations_df_200"
-
-#' Weather stations points in Southwest Nova Scotia
-#' 
-#' A spatial object with one point for each station 
-'swns_stations_sp'
-
 
 
